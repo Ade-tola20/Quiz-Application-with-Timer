c5de044197dd6b0f12da5498fc91f1f95cf01d24
Oduwole Faruq Adetola <oduwoleadetohlar@gmail.com>
2023-09-27 14:29:20 +0100

    Upload

---
+++ js/questions.js
@@ -0,0 +1,127 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which country won the first ever World Cup in 1930?",
+    answer: "Uruguay",
+    options: [
+      "Brazil",
+      "Germany",
+      "Uruguay",
+      "France"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Ronaldo is synonymous with the No.7, but what other number did he wear at Real Madrid?",
+    answer: "9",
+    options: [
+      "8",
+      "10",
+      "11",
+      "9"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which club is associated with 'Galacticos'?",
+    answer: "Real Madrid",
+    options: [
+      "Real Madrid",
+      "Barcelona",
+      "River Plate",
+      "Manchester City"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which player scored the fastest hat-trick in the Premier League?",
+    answer: "Sadio Mane",
+    options: [
+      "Sadio Mane",
+      "Alan Sheara",
+      "Andy Caroll",
+      "Wayne Rooney"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Messi has won a record number of Ballon d'Or awards - how many?",
+    answer: "6",
+    options: [
+      "8",
+      "5",
+      "6",
+      "7"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Which city is home to two La Liga clubs?",
+    answer: "Madrid",
+    options: [
+      "Seville",
+      "Barcelona",
+      "Madrid",
+      "Bilbao"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Former Ballon d'Or winner Andriy Shevchenko is from which nation?",
+    answer: "Ukraine",
+    options: [
+      "Belarus",
+      "Russia",
+      "Estonia",
+      "Ukraine"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Who won the man of the match award for the 2014 World Cup final?",
+    answer: "Mario Götze",
+    options: [
+      "Mario Götze",
+      "Sergio Romero",
+      "Martín Demichelis",
+      "Lionel Messi"
+    ]
+  },
+    {
+    numb: 9,
+    question: "Who was the first non-European to win the Ballon d'Or?",
+    answer: "George Weah",
+    options: [
+      "Ronaldo",
+      "Pelé",
+      "Rivaldo",
+      "George Weah"
+    ]
+  },
+    {
+    numb: 10,
+    question: "What Is The Nickname Of Manchester United?",
+    answer: "The Red Devils",
+    options: [
+      "The Red Lions",
+      "The Gunners",
+      "The Old Lady",
+      "The Red Devils"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
